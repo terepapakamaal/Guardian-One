e5adf7aacc976e0a9fe52c04a718d094af6a760f
Time_Traveller <152459170+terepapakamaal@users.noreply.github.com>
2025-04-07 18:31:55 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -2,11 +2,12 @@ import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import './App.css';
 import Navbar from './components/Navbar';
-import BottomNav from './components/BottomNav'; // Import BottomNav from its own file
-import Home from './pages/Home';
+import BottomNav from './components/BottomNav';
+import HomePage from './components/HomePage';
+import LandingPage from './components/LandingPage';
 import Awareness from './pages/Awareness';
-import SmsSpamCheck from './pages/SmsSpamCheck';
-import URLScanner from './pages/URLScanner';
+import SmsSpamCheck from './components/SmsSpamCheck';
+import URLScanner from './components/URLScanner';
 import PasswordCenter from './pages/PasswordCenter';
 import Auth from './pages/Auth';
 
@@ -15,17 +16,17 @@ function App() {
     <Router>
       <Navbar />
       <Routes>
-        <Route path="/" element={<Home />} />
+        <Route path="/" element={<LandingPage />} />
+        <Route path="/home" element={<HomePage />} />
         <Route path="/awareness" element={<Awareness />} />
         <Route path="/spam" element={<SmsSpamCheck />} />
         <Route path="/url" element={<URLScanner />} />
         <Route path="/password" element={<PasswordCenter />} />
         <Route path="/auth" element={<Auth />} />
       </Routes>
-     <BottomNav/>
+      <BottomNav />
     </Router>
   );
 }
 
 export default App;
-
