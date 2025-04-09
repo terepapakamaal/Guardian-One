015ceddbc65f3e71a4e2cc2c3db79b09df3f3286
Time_Traveller <152459170+terepapakamaal@users.noreply.github.com>
2025-04-09 13:46:08 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,26 +1,26 @@
 import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import './App.css';
-import Navbar from './components/Navbar';
+import ResponsiveAppBar from './components/Navbar'; // Corrected import for Navbar
 import BottomNav from './components/BottomNav';
 import HomePage from './components/HomePage';
 import LandingPage from './components/LandingPage';
 import Awareness from './pages/Awareness';
 import SmsSpamCheck from './components/SmsSpamCheck';
-import URLScanner from './components/URLScanner';
+import UrlScannerPage from './components/UrlScannerPage'; // Corrected import for URLScanner
 import PasswordCenter from './pages/PasswordCenter';
 import Auth from './pages/Auth';
 
 function App() {
   return (
     <Router>
-      <Navbar />
+      <ResponsiveAppBar /> {/* Updated component name */}
       <Routes>
         <Route path="/" element={<LandingPage />} />
         <Route path="/home" element={<HomePage />} />
         <Route path="/awareness" element={<Awareness />} />
         <Route path="/spam" element={<SmsSpamCheck />} />
-        <Route path="/url" element={<URLScanner />} />
+        <Route path="/url" element={<UrlScannerPage />} /> {/* Updated component name */}
         <Route path="/password" element={<PasswordCenter />} />
         <Route path="/auth" element={<Auth />} />
       </Routes>
